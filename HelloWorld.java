75e8da61f20fd1e2d5885d3968eb82082494b9c3
Prajusha <prajushaa@yahoo.com>
2025-11-02 20:52:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 package com.example;
 public class HelloWorld {
 public static void main(String[] args) {
-System.out.println(&quot;Hello, DevOps CI/CD using GitHub Actions!&quot;);
+System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
 }
 }
